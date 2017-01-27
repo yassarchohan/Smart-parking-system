e604b6983e3559d7ccc572a4cde562c9b1789cfa
YassarChohan <kingyassarchohan47@gmail.com>
2017-01-27 06:25:35 -0800

    Smart parking Ui and Schema is designed

---
+++ smart_parking/app/src/main/java/com/example/ubitian/smart_parking/SeeSlots.java
@@ -0,0 +1,29 @@
+package com.example.ubitian.smart_parking;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SeeSlots extends Fragment {
+
+
+    public SeeSlots() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_see_slots, container, false);
+    }
+
+}
